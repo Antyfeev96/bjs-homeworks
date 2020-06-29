6b2c9bd9bfc07319840e346fa5bac101f4fbccaf
Dmitri <griizly@rambler.ru>
2020-06-29 13:34:28 +0300

    Second change

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,10 +1,8 @@
 "use strict";
 function getResult(a,b,c){
     let d = b * b - 4 * a * c;
-    let x;
-    if (d < 0) {
-      x = [];
-    } else if (d == 0) {
+    let x = [];
+    if (d == 0) {
         x = [-b / (2 * a)];
     } else if (d > 0) {
         x = [-b + Math.sqrt(d) / (2 * a),-b - Math.sqrt(d) / (2 * a)];
@@ -18,15 +16,32 @@ function getAverageMark(marks){
         averageMark = 0;
         console.log(averageMark);
     } else if (marks.length >= 5) {
-        console.log('Количество оценок больше пяти');
         marks.splice(5);
-        let sumMarks = marks[0]+marks[1]+marks[2]+marks[3]+marks[4];
-        averageMark = sumMarks/marks.length;
+        let numberSumMarks = 0;
+        let sumMarks = 0;
+        for (let i = 0; i <= marks.length; i++) {
+            numberSumMarks = Number(sumMarks);
+            sumMarks += marks[i];
+        }
+        averageMark = numberSumMarks/marks.length;
     }
     return(averageMark);
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь;
-    // return result;
+    let numberDateOfbirthday = 0;
+    let userAge = 0;
+    numberDateOfbirthday = Number(dateOfBirthday);
+    numberDateOfbirthday = numberDateOfbirthday/31536000000;
+    let currentYear = new Date().getFullYear();
+    console.log(currentYear);
+    console.log(numberDateOfbirthday);
+    userAge = currentYear - numberDateOfbirthday;
+    console.log(userAge);
+    if (userAge < 18){
+        console.log(`Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`)
+    } else {
+        console.log(`Не желаете ли олд-фэшн, ${name}?`)
+    }
+    return(userAge);
 }

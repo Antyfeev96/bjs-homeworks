b248b7a3adab7748e8edef01614e4a9fe63ed335
Dmitri <griizly@rambler.ru>
2020-07-09 17:20:41 +0300

    So hard

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -5,38 +5,37 @@ function getSolutions(a, b, c) {
         roots: []
     };
     if (D < 0) {
-        return(showResult);
+        return showResult;
     } else if (D == 0) {
         const x1 = -b/2 * a;
         showResult.roots.push(x1);
-        return(showResult);
+        return showResult;
     } else {
         const x1 = (-b + Math.sqrt(D))/2 * a;
         const x2 = (-b - Math.sqrt(D))/2 * a;
         showResult.roots.unshift(x1, x2);
-        return(showResult); 
+        return showResult; 
     }
 }
 
+
 function showSolutionsMessage(a, b, c) {
-    let result = {
-        D: showResult.D,
-        roots: showResult.roots
-    };
-    console.log(`Вычисляем корни квадратного уравнения ${a * x** + b * x + c}`);
-    console.log(`Значение дискриминанта: ${D}`);
+    let result = getSolutions(a, b, c);
+    let numberX = Number('x');
+    console.log('Вычисляем корни квадратного уравнения ' + a + numberX * numberX + ' ' + b + numberX + ' ' + c);
+    console.log(`Значение дискриминанта: ${result.D}`);
     if (result.D < 0) {
        return('Уравнение не имеет вещественных корней');
     } else if (result.D == 0) {
-        return(`Уравнение имеет один корень X1 = ${x1}`);
+        return(`Уравнение имеет один корень X1 = ${result.x1}`);
     } else {
-        return(`Уравнение имеет два корня. X1 =  ${x1}, X2 = ${x2}`);
+        return(`Уравнение имеет два корня. X1 =  ${result.x1}, X2 = ${result.x2}`);
     }
 }
 
-function getAverageScore(data) {
+  function getAverageScore(data) {
     let data = {
-        algebra: [],
+       algebra: [],
         average: [],
         chemistry: [],
         english: [],
@@ -48,15 +47,51 @@ function getAverageScore(data) {
         russian: [],
     }
     
-    for (let i = 0; i < data[i].length; i++) {
-        data[i] += data[i][i]/data[i].length;
-        console.log(data[i]);
-    }
     for (let i = 0; i < data.length; i++) {
-        data[i] = data[i] 
+
     }
 }
 
-function getAverageMark(marks) {
-
+function getAverageMark(marks) { 
+    for (let i = 0; i < data[i].length; i++) {
+        data[1][i] += data[1][i]/data[i].length;
+        console.log(data[i]);
+    }
+    for (let i = 0; i < data[i].length; i++) {
+        data[3][i] += data[3][i]/data[i].length;
+        console.log(data[i]);
+    }
+    for (let i = 0; i < data[i].length; i++) {
+        data[4][i] += data[4][i]/data[i].length;
+        console.log(data[i]);
+    }
+    for (let i = 0; i < data[i].length; i++) {
+        data[5][i] += data[5][i]/data[i].length;
+        console.log(data[i]);
+    }
+    for (let i = 0; i < data[i].length; i++) {
+        data[6][i] += data[6][i]/data[i].length;
+        console.log(data[i]);
+    }
+    for (let i = 0; i < data[i].length; i++) {
+        data[7][i] += data[7][i]/data[i].length;
+        console.log(data[i]);
+    }
+    for (let i = 0; i < data[i].length; i++) {
+        data[8][i] += data[8][i]/data[i].length;
+        console.log(data[i]);
+    }
+    for (let i = 0; i < data[i].length; i++) {
+        data[9][i] += data[9][i]/data[i].length;
+        console.log(data[i]);
+    }
+    for (let i = 0; i < data[i].length; i++) {
+        data[10][i] += data[10][i]/data[i].length;
+        console.log(data[i]);
+    }
+    for (let i = 0; i < data[i].length; i++) {
+        data[2] += data[i]/data[i].length;
+        console.log(data[i]);
+    }
+    return data;
 }

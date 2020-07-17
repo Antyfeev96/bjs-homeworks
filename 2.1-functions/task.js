948bcd4873ee224c73333cb575b2cd015c5ace74
Dmitri <griizly@rambler.ru>
2020-07-17 14:05:26 +0300

    Fifth try

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -1,3 +1,5 @@
+//Задание №1
+
 function getSolutions(a, b, c) {
     const D = b * b - 4 * a * c;
     let showResult = {
@@ -36,6 +38,7 @@ function showSolutionsMessage(a, b, c) {
     }
 }
 
+//Задание №2
 
 let data = {
     algebra: [2,4,5,2,3,4],
@@ -49,34 +52,60 @@ let data = {
     russian: [3,3,4,5],
  }
 
-  function getAverageScore(data) {
-    function getAverageMark(marks) {
-        let sum = 0;
-        for (let i = 0; i < marks.length; i++) {
-            sum += marks[i];
-        }
-        return sum/marks.length;
+ function getAverageMark(marks) {
+    if (marks.length === 0) {
+        return 0;
     }
-    
-    let sumAverage = {
-      average: []
+    let sum = 0;
+    for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
     }
-    for (let k = 0; k < data.length; k++) {
-        sumAverage.average += data[k];
-    }
-    data.average = sumAverage.average;
-    console.log(sumAverage.average);
+    return sum/marks.length;
+}
 
-    let result = {
 
+ function getAverageScore(data) {
+    let result = {};
+
+    for (let subject in data) {
+        result[subject] = getAverageMark(data[subject]);
     }
-    if (data = 0) {
-       data.average = 0; 
-       return data.average; 
-    } else {
-    for (const subject in data) {
-        result[subject] = getAverageMark(subject);
+
+    result.average = getAverageMark(Object.values(result)); // Присваивание для result.average значения результата функции getAverageMark, которая перебирает все значения объекта result и по очереди включает это значение в getAverageMark
+    return result; 
+}
+
+
+//Задание №3
+
+let secretData = {
+    aaa: 0,
+    bbb: 0,
+}
+
+function getPersonData(secretData) {
+    let secret = {
+        aaa: 0,
+        bbb: 0,
     }
-    return result;
-} 
+    secretData = secret;
+
+function getDecodedValue(secret) {
+   if (secret.aaa === 0 && secret.bbb === 0 ) {
+       secret.aaa = 'Родриго';
+       secret.bbb = 'Родриго';
+     }else if (secret.aaa === 0 && secret.bbb === 1) {
+       secret.aaa = 'Родриго';
+       secret.bbb = 'Эмильо'; 
+     }else if (secret.aaa === 1 && secret.bbb === 0) {
+       secret.aaa = 'Эмильо';
+       secret.bbb = 'Родриго'; 
+     }else {
+        secret.aaa = 'Эмильо';
+        secret.bbb = 'Эмильо';
+     }
+    return ({firstname: secret.aaa,
+            lastname: secret.bbb});
+}
+ getDecodedValue(secret);
 }

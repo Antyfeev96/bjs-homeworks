d640a75c2afaebabe0e733b1a9aaa3c336c4a705
Dmitri <griizly@rambler.ru>
2020-07-22 18:52:28 +0300

    First try

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,5 +1,21 @@
 //String.prototype.isPalindrome - для задачи №1
 
+String.prototype = isPalindrome;
+
+ function isPalindrome(checkedString) {
+    let stringLen = checkedString.length;
+    let finalStr = checkedString.toLowerCase();
+    for (let i = 0; i < stringLen / 2; i++) {
+        if (finalStr.charAt(i) === finalStr.charAt(stringLen - i - 1)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
+
+
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
     // return averageMark
